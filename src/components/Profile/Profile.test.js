9528f5eff92045f36b0f4728bf55f5283329f2ba
FallenSavior07 <79794180+FallenSavior07@users.noreply.github.com>
2021-08-22 22:53:31 +0300

    написал тесты для компонента ProfileContainer

---
+++ src/components/Profile/Profile.test.js
@@ -0,0 +1,51 @@
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { changeIsOnline, changeName } from '../../actions/profile';
+import { render } from "@testing-library/react";
+import ProfileContainer from './ProfileContainer';
+import { Provider } from 'react-redux';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+jest.mock("react-redux", () => ({
+	useDispatch: jest.fn(),
+	useSelector: jest.fn()
+}))
+
+describe('Profile test', () => {
+	// it('matches snapshot online', () => {
+
+	// 	let state = {};
+	// 	const store = mockStore(() => state);
+
+	// 	const component = render(
+	// 		<Provider store={store}>
+	// 			<ProfileContainer />
+	// 		</Provider>
+	// 	);
+
+	// 	expect(component).toMatchSnapshot();
+	// });
+
+
+	it('dispatches changeName', () => {
+		let state = {};
+		const store = mockStore(() => state);
+		store.dispatch(changeName('John'));
+
+		const actions = store.getActions().map(({ type, payload }) => ({ type, payload }));
+		const expectedActions = [{ type: 'PROFILE::CHANGE_NAME', payload: { name: 'John' } }];
+		expect(actions).toEqual(expectedActions);
+	});
+
+	it('dispatches changeIsOnline', () => {
+		let state = {};
+		const store = mockStore(() => state);
+		store.dispatch(changeIsOnline('false'));
+
+		const actions = store.getActions().map(({ type, payload }) => ({ type, payload }));
+		const expectedActions = [{ type: 'PROFILE::CHANGE_IS_ONLINE', payload: { isOnline: 'false' } }];
+		expect(actions).toEqual(expectedActions);
+	});
+})
